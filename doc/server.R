182b122d5d19021f659b6cbca40d9cd7fe554681
Huijun Cui <32715320+Practical-Theorist@users.noreply.github.com>
2018-04-27 14:46:18 -0400

    Add files via upload

---
+++ doc/server.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(ggplot2)
+library(openxlsx)
+
+function(input, output) {
+  #options(shiny.sanitize.errors = TRUE)
+  terrorism <- read.xlsx("../data/raw data/globalterrorismdb_0617dist.xlsx")
+  terrorism <- terrorism[,c("iyear","imonth", "iday", "country_txt", "region_txt", "provstate", "city", "latitude", "longitude", "attacktype1_txt", "targtype1_txt",  "natlty1_txt",  "weaptype1_txt", "weapsubtype1_txt")]
+  terrorism1<-terrorism[terrorism$iyear<=1974|terrorism$iyear>=2012,]
+ 
+  
+  dataset <- reactive({
+    terrorism1[sample(nrow(terrorism1), input$sampleSize),]
+  })
+  
+  output$plot <- renderPlot({
+    
+    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+    
+    if (input$color != 'None')
+      p <- p + aes_string(color=input$color)
+    
+    if (input$jitter)
+      p <- p + geom_jitter()
+    if (input$smooth)
+      p <- p + geom_smooth()
+    
+    print(p)
+    
+  }, height=700)
+  
+}
---
+++ doc/ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(ggplot2)
+library(openxlsx)
+
+terrorism <- read.xlsx("../data/raw data/globalterrorismdb_0617dist.xlsx")
+terrorism <- terrorism[,c("iyear","imonth", "iday", "country_txt", "region_txt", "provstate", "city", "latitude", "longitude", "attacktype1_txt", "targtype1_txt",  "natlty1_txt", "weaptype1_txt", "weapsubtype1_txt")]
+dataset<-terrorism[terrorism$iyear<=1974|terrorism$iyear>=2012,]
+
+
+fluidPage(
+  
+  titlePanel("Terrorism attacks"),
+  
+  sidebarPanel(
+    
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(1000, nrow(dataset)), step=500, round=0),
+    
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+    
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth')
+    
+  ),
+  
+  
+  mainPanel(
+    plotOutput('plot')
+  )
+)
